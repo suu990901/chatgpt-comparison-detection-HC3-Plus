87d2d07292b3313e500d2e855ee3f5e91fbc8005
suu990901 <149287918+suu990901@users.noreply.github.com>
2024-04-03 22:17:31 +0800

    Update train_chinese_roberta.sh

--- train_chinese_roberta.sh
+++ train_chinese_roberta.sh
@@ -1,5 +1,4 @@
 name=chinese_roberta
-export http_proxy=http://oversea-squid2.ko.txyun:11080 https_proxy=http://oversea-squid2.ko.txyun:11080 no_proxy=localhost,127.0.0.1,localaddress,localdomain.com,internal,corp.kuaishou.com,test.gifshow.com,staging.kuaishou.com
 CUDA_VISIBLE_DEVICES=7 nohup /share/miniconda3/envs/llama/bin/python run_roberta.py \
 --train_path data/zh/train.jsonl \
 --hc3_val_path data/zh/val_hc3_QA.jsonl \
