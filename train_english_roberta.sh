2b8035c9d1c519d579c339efceb1b29d60dc49d3
suu990901 <149287918+suu990901@users.noreply.github.com>
2024-04-03 22:18:22 +0800

    Update train_english_roberta.sh

--- train_english_roberta.sh
+++ train_english_roberta.sh
@@ -1,5 +1,4 @@
 name=english_roberta
-export http_proxy=http://oversea-squid2.ko.txyun:11080 https_proxy=http://oversea-squid2.ko.txyun:11080 no_proxy=localhost,127.0.0.1,localaddress,localdomain.com,internal,corp.kuaishou.com,test.gifshow.com,staging.kuaishou.com
 CUDA_VISIBLE_DEVICES=6 nohup /share/miniconda3/envs/llama/bin/python run_roberta.py \
 --train_path data/en/train.jsonl \
 --hc3_val_path data/en/val_hc3_QA.jsonl \
